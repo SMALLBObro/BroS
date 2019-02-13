5fde7aa735324d0a81ab899f81fd3086a82a7873
Kickqwa <dima_s@rambler.ru>
2019-02-13 19:45:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,46 +4,46 @@
 
 using namespace std;
 
-char player1, player2, player3, player4, player5, player6, player7;
+char p1, p2, p3, p4, p5, p6, p7;
 
-struct gamer {
+/*struct gg {
 	char name[10];
 	int role;
-	gamer* next;
+	gg* next;
 };
 
-class game_list {
+class g_list {
 public:
-	gamer* head; // Первый элемент (голова) списка
-	gamer* tail; // Последний элемент (хвост) списка
-	game_list();
-	~game_list();
+	gg* head; // Первый элемент (голова) списка
+	gg* tail; // Последний элемент (хвост) списка
+	g_list();
+	~g_list();
 	bool chk_empty();
 	void comp_in(char* n, int r);
-	gamer* search(char *n);
-	void comp_edit(gamer &c, int* r);
+	gg* search(char *n);
+	void comp_edit(gg &c, int* r);
 	void print();
-	void comp_del(gamer* c);
+	void comp_del(gg* c);
 	void write_in_file();
 };
 
-game_list::game_list() : head(nullptr), tail(nullptr) {}
+g_list::g_list() : head(nullptr), tail(nullptr) {}
 
-game_list::~game_list() {
+g_list::~g_list() {
 	while (head != 0) {
-		gamer* c = head;
+		gg* c = head;
 		head = head->next;
 		delete c;
 	}
 }
 
-bool game_list::chk_empty()
+bool g_list::chk_empty()
 {
 	return (head == nullptr);
 }
 
-void game_list::comp_in(char* n, int r) {
-	gamer* c = new gamer();
+void g_list::comp_in(char* n, int r) {
+	gg* c = new gg();
 	int nn;
 	strcpy_s(c->name, 20, n);
 	c->role = r;
@@ -54,11 +54,12 @@ void game_list::comp_in(char* n, int r) {
 		tail->next = c;
 	tail = c;
 }
-
+*/
 class role {
-	int role[7] = { 0, 1, 2, 3, 4, 5, 6 }; //0-hit;1-2-red;3-6-blue
+public:
+	int roles[7] = { 0, 1, 2, 3, 4, 5, 6 }; //0-hit;1-2-red;3-6-blue
 	int used_role[7];
-	char players[7] = { player1, player2, player3, player4, player5, player6, player7 };
+	char pls[7] = { p1, p2, p3, p4, p5, p6, p7 };
 	int test_role;
 	int used_num = 0;
 	bool fucked;
@@ -66,32 +67,28 @@ class role {
 };
 
 void role::choice_role() {
-
-	for (int i = 0; i < 7; i++) {
-		
-	}
-
 	for (int i = 0; i < 7; i++) {
 		do {
 			fucked = false;
-			test_role = role[rand() % 7];
+			test_role = roles[rand() % 7];
 			for (int j = 0; j < used_num; j++) {
 				if (test_role == used_role[j])
-					fucked = true;				
+					fucked = true;
 			}
 		} while (fucked == true);
 
-		players[i] = test_role;
+		pls[i] = test_role;
 		used_role[used_num] = test_role;
 		used_num++;
-		cout << players[i] << " = " << endl;
-	}	   
+		cout << pls[i] << " = " << endl;
+	}
 }
 
 int main() {
 	setlocale(LC_ALL, "Rus");
 	srand((unsigned)time(NULL));
 
+	//g_list g;
 	role r;
 
 	r.choice_role();
